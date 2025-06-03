48938727485e9df8eccb21a3499e43da785c3e3c
Hadar Cohen <hacohen@redhat.com>
2025-06-03 17:36:13 +0300

    remove unused yamls

--- oc_apply.sh
+++ oc_apply.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 oc apply -f openshift/tasks/preprocess-task.yaml
-# oc apply -f openshift/taskruns/preprocess-taskrun.yaml
 oc apply -f openshift/tasks/train-task.yaml
-# oc apply -f openshift/taskruns/train-taskrun.yaml
 
 oc apply -f openshift/tasks/pipeline.yaml
 oc apply -f openshift/taskruns/pipelinerun.yaml
