b9c6dfc17bf3e9ca6c258307de59111f919103a9
Hadar Cohen <hacohen@redhat.com>
2025-06-03 12:24:19 +0300

    format document

--- data/preprocess.py
+++ data/preprocess.py
@@ -10,15 +10,14 @@ def load_and_preprocess_data() -> pd.DataFrame:
     Load data from a sickit-learn dataset and preprocess it.
     """
 
-    
     # Load dataset
     logger.info("Loading and preprocessing data")
-    df = sns.load_dataset('titanic')
+    df = sns.load_dataset("titanic")
     logger.debug(f"Dataset loaded successfully, it has the shape of {df.shape}")
 
     # Drop irrelevant or high-missing-value columns
     logger.info("Dropping irrelevant columns")
-    df = df.drop(columns=['deck', 'embark_town', 'alive', 'class', 'who'])
+    df = df.drop(columns=["deck", "embark_town", "alive", "class", "who"])
 
     # Drop rows with null values
     logger.info("Dropping rows with null values")
@@ -28,20 +27,21 @@ def load_and_preprocess_data() -> pd.DataFrame:
     # Encode categorical variables
     logger.info("Encoding categorical variables")
     label_encoders = {}
-    for col in df.select_dtypes(include='object').columns:
+    for col in df.select_dtypes(include="object").columns:
         le = LabelEncoder()
         df[col] = le.fit_transform(df[col])
         label_encoders[col] = le
 
     # Split features and label
-    X = df.drop(columns=['survived'])
-    y = df['survived']
+    X = df.drop(columns=["survived"])
+    y = df["survived"]
 
     # Train-test split
     logger.info("Splitting the data into training and testing sets")
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
     logger.debug(f"Shape of X_train: {X_train.shape}, Shape of X_test: {X_test.shape}")
-    
 
     # Save to disk
     logger.info("Saving preprocessed data to disk")
