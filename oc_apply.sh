bce93b8e1afc56168148bf3b0a821c6df205e1be
Hadar Cohen <hacohen@redhat.com>
2025-06-04 12:00:41 +0300

    add build and deploy

--- oc_apply.sh
+++ oc_apply.sh
@@ -2,6 +2,8 @@
 
 oc apply -f openshift/tasks/preprocess-task.yaml
 oc apply -f openshift/tasks/train-task.yaml
+oc apply -f openshift/tasks/build-task.yaml     
+oc apply -f openshift/tasks/deploy-task.yaml 
 
 oc apply -f openshift/tasks/pipeline.yaml
 oc apply -f openshift/taskruns/pipelinerun.yaml
