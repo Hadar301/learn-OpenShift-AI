7d8530c6eece409543864422981f51e3446d7727
Hadar Cohen <hacohen@redhat.com>
2025-06-04 12:01:54 +0300

    delete existing PVCs

--- oc_apply.sh
+++ oc_apply.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+## delete pvc
+oc get pvc --no-headers -o custom-columns=NAME:.metadata.name \
+        | grep '^shared-pvc-titanic' \
+        | xargs -r oc delete pvc
+
 oc apply -f openshift/tasks/preprocess-task.yaml
 oc apply -f openshift/tasks/train-task.yaml
 oc apply -f openshift/tasks/build-task.yaml     
