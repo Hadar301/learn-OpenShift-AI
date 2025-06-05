f7253d9dfcb30ddb3915f34c19f7b033a6b1da07
Hadar Cohen <hacohen@redhat.com>
2025-06-05 15:44:44 +0300

    update script to release resources

--- oc_apply.sh
+++ oc_apply.sh
@@ -3,10 +3,8 @@
 ## Optional - delete the pipeline
 # oc delete pipelinerun train-pipeline-run -n rh-ee-hacohen-dev  
 
-## delete pvc
-oc get pvc --no-headers -o custom-columns=NAME:.metadata.name \
-        | grep '^shared-pvc-titanic' \
-        | xargs -r oc delete pvc &
+## relese the resources that were captured by previous pipelineruns:
+oc get pipelinerun -o name | grep '^pipelinerun.train-pipeline-run' | xargs oc delete
 
 oc apply -f openshift/tasks/preprocess-task.yaml
 oc apply -f openshift/tasks/train-task.yaml
